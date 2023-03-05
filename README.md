9c9bb19ba5793e847acebb32d1fc44ae6c216ae2
reeeeverb <skydoessoftware@gmail.com>
2023-03-05 11:27:06 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Auto-Castle
+
+## Chessboard TODO:
+- Recreate to allow for programmatic adding of widgets(pieces)
+- Implement pawn promotion
+  - need a fancy lil mini gui
+- Check, checkmate, etc
+  - Checkmate still needed
+    - legal moves array?
+  - Check from other kings
+- Stalemate
+- Timers
---
+++ main.py
@@ -0,0 +1,17 @@
+import math
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
+from kivy.vector import Vector
+from kivy.uix.image import Image
+from kivy.core.window import Window
+from kivy.graphics import Rectangle
+from kivy.uix.button import Button
+from kivy.lang.builder import Builder
+
+if __name__ == '__main__':
+    ChessApp().run()
+
+class ChessApp(App):
+    def build(self):
+        return ChessGame()
