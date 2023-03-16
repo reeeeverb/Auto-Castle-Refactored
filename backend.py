84675ce9de73e160bc7ee021b9cf908faf49a61f
reverb <skydoessoftware@gmail.com>
2023-03-16 04:11:10 -0500

    check moves

--- backend.py
+++ backend.py
@@ -1,3 +1,4 @@
+import copy
 import math
 from presets import *
 import generate
@@ -15,18 +16,37 @@ class board():
     w_king_castle = True
     b_queen_castle = True
     b_king_castle = True
+    w_king_location = None
+    b_king_location = None
     move = 0
 
     def reset_board(self):
         self.set_board(new_board_w_piece,new_board_w_color)
         self.queen_castle = True
         self.king_castle = True
+        self.w_king_location = 3
+        self.b_king_location = 59
 
     def set_board(self,loc,col):
         self.color_arr = col
         self.piece_arr = loc
     
     def move_piece(self,down_square,up_square):
+        t_color_arr = copy.copy(self.color_arr)
+        t_piece_arr = copy.copy(self.piece_arr)
+        t_forward = copy.copy(self.forward)
+        t_current_move = copy.copy(self.current_move)
+        t_w_en_passantable = copy.copy(self.w_en_passantable)
+        t_b_en_passantable = copy.copy(self.b_en_passantable)
+        t_w_en_passantable_s = copy.copy(self.w_en_passantable_s)
+        t_b_en_passantable_s = copy.copy(self.b_en_passantable_s)
+        t_w_queen_castle =  copy.copy(self.w_queen_castle)
+        t_w_king_castle = copy.copy(self.w_king_castle)
+        t_b_queen_castle = copy.copy(self.b_queen_castle)
+        t_b_king_castle = copy.copy(self.b_king_castle)
+        t_w_king_location = copy.copy(self.w_king_location)
+        t_b_king_location = copy.copy(self.b_king_location)
+            
         piece = self.piece_arr[down_square]
         if piece == "NOTHING":
             return;
@@ -43,22 +63,24 @@ class board():
         self.b_en_passantable = None
         if piece == "KING":
             if color == "WHITE":
-                w_queen_castle = False
-                w_king_castle = False
+                self.w_king_location = up_square
+                self.w_queen_castle = False
+                self.w_king_castle = False
             elif color == "BLACK":
-                b_queen_castle = False
-                b_king_castle = False
+                self.b_king_location = up_square
+                self.b_queen_castle = False
+                self.b_king_castle = False
         elif piece == "ROOK":
             if color == "WHITE":
                 if down_square == 0 or down_square == 56:
-                    w_king_castle = False
+                    self.w_king_castle = False
                 elif down_square == 7 or down_square == 63:
-                    w_queen_castle = False
+                    self.w_queen_castle = False
             elif color == "BLACK":
                 if down_square == 0 or down_square == 56:
-                    b_king_castle = False
+                    self.b_king_castle = False
                 elif down_square == 7 or down_square == 63:
-                    b_queen_castle = False
+                    self.b_queen_castle = False
         elif piece == "PAWN":
             if up_square - down_square == 16:
                 if color == "WHITE":
@@ -85,5 +107,36 @@ class board():
         self.piece_arr[down_square] = "EMPTY"
         self.color_arr[up_square] = color
         self.piece_arr[up_square] = piece
-        self.move += 1
-        self.current_move = "WHITE" if color == "BLACK" else "BLACK"
+        if self.current_move == "WHITE" and generate.in_check(self.w_king_location,self):
+            self.color_arr = t_color_arr
+            self.piece_arr = t_piece_arr
+            self.forward = t_forward
+            self.current_move = t_current_move
+            self.w_en_passantable = t_w_en_passantable
+            self.b_en_passantable = t_b_en_passantable
+            self.w_en_passantable_s = t_w_en_passantable_s
+            self.b_en_passantable_s = t_b_en_passantable_s
+            self.w_queen_castle =  t_w_queen_castle
+            self.w_king_castle = t_w_king_castle
+            self.b_queen_castle = t_b_queen_castle
+            self.b_king_castle = t_b_king_castle
+            self.w_king_location = t_w_king_location
+            self.b_king_location = t_b_king_location
+        elif self.current_move == "BLACK" and generate.in_check(self.b_king_location,self):
+            self.color_arr = t_color_arr
+            self.piece_arr = t_piece_arr
+            self.forward = t_forward
+            self.current_move = t_current_move
+            self.w_en_passantable = t_w_en_passantable
+            self.b_en_passantable = t_b_en_passantable
+            self.w_en_passantable_s = t_w_en_passantable_s
+            self.b_en_passantable_s = t_b_en_passantable_s
+            self.w_queen_castle =  t_w_queen_castle
+            self.w_king_castle = t_w_king_castle
+            self.b_queen_castle = t_b_queen_castle
+            self.b_king_castle = t_b_king_castle
+            self.w_king_location = t_w_king_location
+            self.b_king_location = t_b_king_location
+        else:
+            self.move += 1
+            self.current_move = "WHITE" if color == "BLACK" else "BLACK"
--- generate.py
+++ generate.py
@@ -176,3 +176,232 @@ def moves(square,board):
             if row > 0 and col > 0 and color_arr[square-9] != color:
                 out.append(square-9)
     return out
+
+def in_check(square,board):
+        color = board.color_arr
+        piece = board.piece_arr
+
+        if color[square] == "WHITE":
+            white = 1
+        elif color[square] == "BLACK":
+            white = 0
+
+        king_row = square//8
+        king_col = square%8
+
+        # From above check
+        if king_row < 7:
+            temp_row = king_row+1
+            temp_pos = temp_row*8+king_col
+            while temp_row < 7 and piece[temp_pos] == "EMPTY":
+                temp_row+=1;
+                temp_pos+=8;
+            hit_p = piece[temp_pos]
+            hit_c = color[temp_pos]
+            #print("From front: ",hit_c, hit_p)
+            if (hit_c == "BLACK" and white == 1) or (hit_c == "WHITE" and white == 0):
+                if (hit_p == "ROOK" or hit_p == "QUEEN"):
+                    print("In check")
+                    return True
+
+        # From below check
+        if king_row > 0:
+            temp_row = king_row-1
+            temp_pos = temp_row*8+king_col
+            while temp_row > 0 and piece[temp_pos] == "EMPTY":
+                temp_row-=1;
+                temp_pos-=8;
+            hit_p = piece[temp_pos]
+            hit_c = color[temp_pos]
+            #print("From back: ",hit_c, hit_p)
+            if (hit_c == "BLACK" and white == 1) or (hit_c == "WHITE" and white == 0):
+                if (hit_p == "ROOK" or hit_p == "QUEEN"):
+                    print("In check")
+                    return True
+
+        # From right check
+        if king_col < 7:
+            temp_col = king_col+1
+            temp_pos = king_row*8+temp_col
+            while temp_col < 7 and piece[temp_pos] == "EMPTY":
+                temp_col+=1;
+                temp_pos+=1;
+            hit_p = piece[temp_pos]
+            hit_c = color[temp_pos]
+            #print("From right: ",hit_c, hit_p)
+            if (hit_c == "BLACK" and white == 1) or (hit_c == "WHITE" and white == 0):
+                if (hit_p == "ROOK" or hit_p == "QUEEN"):
+                    print("In check")
+                    return True
+
+        # From left check
+        if king_col > 0:
+            temp_col = king_col-1
+            temp_pos = king_row*8+temp_col
+            while temp_col > 0 and piece[temp_pos] == "EMPTY":
+                temp_col-=1;
+                temp_pos-=1;
+            hit_p = piece[temp_pos]
+            hit_c = color[temp_pos]
+            #print("From left: ",hit_c, hit_p)
+            if (hit_c == "BLACK" and white == 1) or (hit_c == "WHITE" and white == 0):
+                if (hit_p == "ROOK" or hit_p == "QUEEN"):
+                    print("In check")
+                    return True
+
+        # From diagonal top right check
+        if king_row < 7 and king_col < 7:
+            temp_count = 1
+            temp_row = king_row+1
+            temp_col = king_col+1
+            temp_pos = temp_row*8+temp_col
+            while temp_col < 7 and temp_row < 7 and piece[temp_pos] == "EMPTY":
+                temp_col+=1;
+                temp_row+=1;
+                temp_pos+=9;
+                temp_count += 1
+            hit_p = piece[temp_pos]
+            hit_c = color[temp_pos]
+            #print("From upper right: ",hit_c, hit_p)
+            if (hit_c == "BLACK" and white == 1) or (hit_c == "WHITE" and white == 0):
+                if (hit_p == "BISHOP" or hit_p == "QUEEN"):
+                    print("In check")
+                    return True
+                elif (temp_count == 1 and (hit_p == "PAWN" or hit_p == "KING") and board.forward != hit_c):
+                    print("Pawn Check")
+                    return True
+
+        # From diagonal top left check
+        if king_row < 7 and king_col > 0:
+            temp_count = 0
+            temp_row = king_row+1
+            temp_col = king_col-1
+            temp_pos = temp_row*8+temp_col
+            while temp_col > 0 and temp_row < 7 and piece[temp_pos] == "EMPTY":
+                temp_col-=1;
+                temp_row+=1;
+                temp_pos+=7;
+                temp_count+=1
+            hit_p = piece[temp_pos]
+            hit_c = color[temp_pos]
+            #print("From upper left: ",hit_c, hit_p)
+            if (hit_c == "BLACK" and white == 1) or (hit_c == "WHITE" and white == 0):
+                if (hit_p == "BISHOP" or hit_p == "QUEEN"):
+                    print("In check")
+                    return True
+                elif (temp_count == 1 and (hit_p == "PAWN" or hit_p == "KING") and board.forward == hit_c):
+                    print("Pawn Check")
+                    return True
+
+        # From diagonal bottom right check
+        if king_row > 0 and king_col < 7:
+            temp_count = 0
+            temp_row = king_row-1
+            temp_col = king_col+1
+            temp_pos = temp_row*8+temp_col
+            while temp_col < 7 and temp_row > 0 and piece[temp_pos] == "EMPTY":
+                temp_col+=1;
+                temp_row-=1;
+                temp_pos-=7;
+            hit_p = piece[temp_pos]
+            hit_c = color[temp_pos]
+            #print("From lower right: ",hit_c, hit_p)
+            if (hit_c == "BLACK" and white == 1) or (hit_c == "WHITE" and white == 0):
+                if (hit_p == "BISHOP" or hit_p == "QUEEN"):
+                    print("In check")
+                    return True
+                elif (temp_count == 1 and (hit_p == "PAWN" or hit_p == "KING") and board.forward != hit_c):
+                    print("Pawn Check")
+                    return True
+
+        # From diagonal bottom left check
+        if king_row > 0 and king_col > 0:
+            temp_count = 0
+            temp_row = king_row-1
+            temp_col = king_col-1
+            temp_pos = temp_row*8+temp_col
+            while temp_col > 0 and temp_row > 0 and piece[temp_pos] == "EMPTY":
+                temp_col-=1;
+                temp_row-=1;
+                temp_pos-=9;
+            hit_p = piece[temp_pos]
+            hit_c = color[temp_pos]
+            #print("From lower left: ",hit_c, hit_p)
+            if (hit_c == "BLACK" and white == 1) or (hit_c == "WHITE" and white == 0):
+                if (hit_p == "BISHOP" or hit_p == "QUEEN"):
+                    print("In check")
+                    return True
+                elif (temp_count == 1 and (hit_p == "PAWN" or hit_p == "KING") and board.forward != hit_c):
+                    print("Pawn Check")
+                    return True
+        # Knight checks
+        if white == 1:
+            temp_c = "WHITE"
+        else:
+            temp_c = "BLACK"
+        if king_row < 7 and king_col > 1:
+            temp_col=king_col-2
+            temp_row=king_row+1
+            temp_pos = temp_row*8+temp_col
+            if piece[temp_pos] == "KNIGHT" and color[temp_pos] != temp_c:
+                print("Check dectected, knight at ", temp_pos)
+                return True
+
+        if king_row < 7 and king_col < 6:
+            temp_col=king_col+2
+            temp_row=king_row+1
+            temp_pos = temp_row*8+temp_col
+            if piece[temp_pos] == "KNIGHT" and color[temp_pos] != temp_c:
+                print("Check dectected, knight at ", temp_pos)
+                return True
+
+        if king_row > 0 and king_col > 1:
+            temp_col=king_col-2
+            temp_row=king_row-1
+            temp_pos = temp_row*8+temp_col
+            if piece[temp_pos] == "KNIGHT" and color[temp_pos] != temp_c:
+                print("Check dectected, knight at ", temp_pos)
+                return True
+
+        if king_row > 0 and king_col < 6:
+            temp_col=king_col+2
+            temp_row=king_row-1
+            temp_pos = temp_row*8+temp_col
+            if piece[temp_pos] == "KNIGHT" and color[temp_pos] != temp_c:
+                print("Check dectected, knight at ", temp_pos)
+                return True
+
+        if king_row > 1 and king_col < 7:
+            temp_col=king_col+1
+            temp_row=king_row-2
+            temp_pos = temp_row*8+temp_col
+            if piece[temp_pos] == "KNIGHT" and color[temp_pos] != temp_c:
+                print("Check dectected, knight at ", temp_pos)
+                return True
+
+        if king_row > 1 and king_col > 0:
+            temp_col=king_col-1
+            temp_row=king_row-2
+            temp_pos = temp_row*8+temp_col
+            if piece[temp_pos] == "KNIGHT" and color[temp_pos] != temp_c:
+                print("Check dectected, knight at ", temp_pos)
+                return True
+
+        if king_row < 6 and king_col > 0:
+            temp_col=king_col-1
+            temp_row=king_row+2
+            temp_pos = temp_row*8+temp_col
+            if piece[temp_pos] == "KNIGHT" and color[temp_pos] != temp_c:
+                print("Check dectected, knight at ", temp_pos)
+                return True
+
+        if king_row < 6 and king_col < 7:
+            temp_col=king_col+1
+            temp_row=king_row+2
+            temp_pos = temp_row*8+temp_col
+            if piece[temp_pos] == "KNIGHT" and color[temp_pos] != temp_c:
+                print("Check dectected, knight at ", temp_pos)
+                return True
+        return False
+
+
